5f1883f6ff94f123d183853f96b4882085555061
Liran <Liran.Sorokin@s.afeka.ac.il>
2024-08-25 01:42:01 +0300

    Added FirebaseAuthUI packages.

--- EasyList/Controllers/ViewController.swift
+++ EasyList/Controllers/ViewController.swift
@@ -10,15 +10,26 @@ import FirebaseCore
 import FirebaseAuth
 import FirebaseDatabase
 import FirebaseAuthUI
+import FirebaseGoogleAuthUI
+import FirebaseEmailAuthUI
 
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, FUIAuthDelegate {
+    
     var ref: DatabaseReference!
+    
+    let providers: [FUIAuthProvider] = [
+        FUIGoogleAuth(),
+        FUIEmailAuth()
+    ]
+    
+    let authUI = FUIAuth.defaultAuthUI()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        FirebaseApp.configure()
         ref = Database.database().reference()
         
         initFirebaseAuthentication()
@@ -26,9 +37,24 @@ class ViewController: UIViewController {
     }
     
     func initFirebaseAuthentication() {
+        self.authUI?.providers = providers
+
+        authUI?.delegate = self
+        
+        let authViewController = authUI?.authViewController()
+    }
+    
+    func authUI(_ authUI: FUIAuth, didSignInWith user: FirebaseAuth.User?, error: Error?) {
         
     }
-
-
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any]) -> Bool {
+        let sourceApplication = options[UIApplication.OpenURLOptionsKey.sourceApplication] as! String?
+        if FUIAuth.defaultAuthUI()?.handleOpen(url, sourceApplication: sourceApplication) ?? false {
+            return true
+        }
+        return false
+    }
+    
 }
 
